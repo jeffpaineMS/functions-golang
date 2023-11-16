9cd30dde8c78c4f42d05cad1b990c2d5f34692d5
Jeff Paine <jeff.paine@gmail.com>
2023-11-16 13:37:24 -0500

    Add go functions

---
+++ handler.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	message := "This HTTP triggered function executed successfully. Pass a name in the query string for a personalized response.\n"
+	name := r.URL.Query().Get("name")
+	if name != "" {
+		message = fmt.Sprintf("Hello, %s. This HTTP triggered function executed successfully.\n", name)
+	}
+	fmt.Fprint(w, message)
+}
+
+func healthHandler(res http.ResponseWriter, r *http.Request) {
+	type Health struct {
+		Status string `json:"status"`
+	}
+
+	health := &Health{
+		Status: "Up",
+	}
+	res.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	err := json.NewEncoder(res).Encode(health)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func main() {
+	listenAddr := ":8080"
+	if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
+		listenAddr = ":" + val
+	}
+	http.HandleFunc("/api/HttpExample", helloHandler)
+	http.HandleFunc("/api/health", healthHandler)
+	log.Printf("About to listen on %s. Go to https://127.0.0.1%s/", listenAddr, listenAddr)
+	log.Fatal(http.ListenAndServe(listenAddr, nil))
+}
