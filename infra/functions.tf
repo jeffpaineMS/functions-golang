51525662cfa0a3811947732e353922763a3e726e
Jeff Paine <jeff.paine@gmail.com>
2023-11-17 10:24:48 -0500

    App insights and diag logs

--- infra/functions.tf
+++ infra/functions.tf
@@ -12,6 +12,23 @@ resource "azurerm_storage_account" "example" {
   account_replication_type = "LRS"
 }
 
+resource "azurerm_log_analytics_workspace" "example" {
+  name                = "jeffs-go-func-law"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_application_insights" "example" {
+  name                = "jeffs-go-func-ai"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  workspace_id        = azurerm_log_analytics_workspace.example.id
+  application_type    = "web"
+}
+
+
 resource "azurerm_service_plan" "example" {
   name                         = "example-app-service-plan"
   resource_group_name          = azurerm_resource_group.example.name
@@ -29,13 +46,16 @@ resource "azurerm_linux_function_app" "example" {
   location            = azurerm_resource_group.example.location
   service_plan_id     = azurerm_service_plan.example.id
 
+
   storage_account_name       = azurerm_storage_account.example.name
   storage_account_access_key = azurerm_storage_account.example.primary_access_key
 
   site_config {
+    application_insights_connection_string = azurerm_application_insights.example.connection_string
     application_stack {
       use_custom_runtime = true
     }
+
   }
 }
 
@@ -46,8 +66,39 @@ resource "azurerm_linux_function_app_slot" "example" {
   storage_account_name       = azurerm_storage_account.example.name
   storage_account_access_key = azurerm_storage_account.example.primary_access_key
   site_config {
+    application_insights_connection_string = azurerm_application_insights.example.connection_string
+
     application_stack {
       use_custom_runtime = true
     }
+  }
+}
+
+resource "azurerm_monitor_diagnostic_setting" "example" {
+  name                       = "example"
+  target_resource_id         = azurerm_linux_function_app.example.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
+
+  enabled_log {
+    category = "FunctionAppLogs"
+  }
+
+  metric {
+    category = "AllMetrics"
+
+  }
+}
+resource "azurerm_monitor_diagnostic_setting" "example" {
+  name                       = "example"
+  target_resource_id         = azurerm_linux_function_app_slot.example.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
+
+  enabled_log {
+    category = "FunctionAppLogs"
+  }
+
+  metric {
+    category = "AllMetrics"
+
   }
 }
